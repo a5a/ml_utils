567ee21bfeef07786a19b7b7b32db0a270c0d76c
Ahsan Alvi <ahsalvi@gmail.com>
2018-10-26 18:36:44 +0100

    test for visualizing the gradient of the posterior

--- tests/models/test_gp.py
+++ tests/models/test_gp.py
@@ -1,6 +1,42 @@
 import numpy as np
 from ml_utils.models import GP
 import GPy
+import pylab as plt
+
+
+def create_data_and_surrogate():
+    x = np.arange(4).astype(float)[:, None]
+    x_dense = np.linspace(np.min(x), np.max(x), 100)[:, None]
+
+    y = 5 * np.sin(x)
+    y -= np.mean(y)
+    kern = GPy.kern.RBF(1)
+
+    hp_bounds = np.array([[1e-6, 1e6],  # kernel variance
+                          [1e-4, 1e3],  # lengthscale
+                          # [1e-6, 1e6]  # likelihood variance
+                          ])
+
+    hyper_priors = [GPy.priors.Gamma(a=1.0, b=0.1),
+                    # lengthscale prior keeps it small-ish
+                    GPy.priors.Gamma(a=1.5, b=1.5),
+                    # GPy.priors.Gamma(a=1.0, b=0.1)
+                    ]
+
+    gp_opt_params = {'method': 'multigrad',
+                     'num_restarts': 10,
+                     'restart_bounds': np.array([[1e-6, 10],  # kernel variance
+                                                 [1e-4, 5],  # lengthscale
+                                                 ]),
+                     'hp_bounds': hp_bounds,
+                     'verbose': False}
+
+    surrogate = GP(x, y, kern, lik_variance=1e-3, lik_variance_fixed=True,
+                   opt_params=gp_opt_params,
+                   hyper_priors=hyper_priors)
+    surrogate.optimize()
+
+    return x, y, surrogate, x_dense
 
 
 def test_predictive_gradients():
@@ -26,6 +62,7 @@ def test_predictive_gradients():
     assert np.allclose(dmu_dx1, dmu_dx2)
     assert np.allclose(dvar_dx1, dvar_dx2)
 
+
 def test_optimization_doesnt_crash():
     x = np.arange(5)[:, None]
     y = np.sin(x)
@@ -35,6 +72,19 @@ def test_optimization_doesnt_crash():
     mygp = GP(x, y, kern, lik_variance=1.)
     mygp.optimize()
 
+
+def test_dmu_dx():
+    x, y, surrogate, x_dense = create_data_and_surrogate()
+    mu = surrogate.predict(x_dense)[0]
+    dmu = surrogate.dmu_dx(x_dense).sum(-1)
+    plt.plot(mu, label='mu')
+    plt.plot(dmu, label='dmu')
+    plt.plot(np.arange(len(mu)), np.zeros(len(mu)), 'k')
+    plt.legend()
+    plt.show()
+
+
 if __name__ == '__main__':
-    test_predictive_gradients()
-    test_optimization_doesnt_crash()
+    # test_predictive_gradients()
+    # test_optimization_doesnt_crash()
+    test_dmu_dx()
