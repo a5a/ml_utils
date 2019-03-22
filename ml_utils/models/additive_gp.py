WIP: additive GP learning... normalising y and fixing variance to 1 seems to be the trick

ml_utils/models/additive_gp.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-
+import pylab as plt
 import GPy
 from typing import Optional, List, Union
 
@@ -16,7 +16,7 @@ class AdditiveGP(GP):
         kernel = self.kern.parameters[subspace_idx]
         mu = kernel.K(x, self.X) @ self.alpha
         var = kernel.K(x, x) - \
-            kernel.K(x, self.X) @ self.Ka_inv @ kernel.K(self.X, x)
+              kernel.K(x, self.X) @ self.Ka_inv @ kernel.K(self.X, x)
 
         if not full_cov:
             var = np.diag(var).reshape(mu.shape)
@@ -107,8 +107,6 @@ class StationaryUniformCat(GPy.kern.Kern):
     categorical kernel. Each cat input has the same weight and is
     the cat variables' contribution to K() is normalised to [0, 1]
 
-    TODO: Convert into GPy.kern.Kern class for optimisation purposes
-    TODO: Need access to parameters (variance, lengthscale)
     TODO: Deal with gradients
     """
 
@@ -130,7 +128,17 @@ class StationaryUniformCat(GPy.kern.Kern):
         k_kernel = self.kernel.K(X, X2)
 
         k_cat = self.K_cat(X, X2)
-        return k_kernel + k_cat
+
+        k_t = k_kernel + k_cat
+        # f, (ax1, ax2, ax3) = plt.subplots(3, 1)
+        # plt.title(f"min_kernel = {np.min(k_kernel)}, max_kernel = {np.max(k_kernel)}, \n"
+        #             f"min_cat = {np.min(k_cat)}, max_cat = {np.max(k_cat)}")
+        # ax1.imshow(k_kernel)
+        # ax2.imshow(k_cat)
+        # ax3.imshow(k_kernel + k_cat)
+        # # f.colorbar()
+        # plt.show()
+        return k_t
 
     def K_cat(self, X, X2):
         X_cat = X[:, self.cat_dims]
@@ -153,7 +161,9 @@ class StationaryUniformCat(GPy.kern.Kern):
 
         # Update the variance gradient using the contribution of the categories
         stat_grad = self.kernel.variance.gradient
-        cat_kern_contrib = np.sum(self.K_cat(X, X2=X2)) / self.kernel.variance
+        cat_kern_contrib = np.sum(self.K_cat(X, X2=X2) * dL_dK) / self.kernel.variance
 
+        # ASA: Add this in again...
         self.kernel.variance.gradient = stat_grad + cat_kern_contrib
-        # print(self.gradient)
+
+        print(f"Updating gradient: {np.hstack((self.gradient, cat_kern_contrib))}")

tests/models/test_additive_gp.py
@@ -95,20 +95,20 @@ def test_subspace_learning():
     plt.show()
 
 
-def test_kernel_with_delta():
-    k_continuous = GPy.kern.RBF(1, active_dims=[0])
-    k = KernelWithDelta(k_continuous, [1])
-    print(k.lengthscale)
-    # DOESN'T WORK :(
-
-
-def test_rbf_with_delta():
-    x = np.arange(10).reshape(-1, 1)
-    x = np.hstack((x, x))
-
-    k = RBFWithDelta([0], [1])
-    k = GPy.kern.RBF(1, active_dims=[0])
-    k.K(x)
+# def test_kernel_with_delta():
+#     k_continuous = GPy.kern.RBF(1, active_dims=[0])
+#     k = KernelWithDelta(k_continuous, [1])
+#     print(k.lengthscale)
+#     # DOESN'T WORK :(
+#
+#
+# def test_rbf_with_delta():
+#     x = np.arange(10).reshape(-1, 1)
+#     x = np.hstack((x, x))
+#
+#     k = RBFWithDelta([0], [1])
+#     k = GPy.kern.RBF(1, active_dims=[0])
+#     k.K(x)
 
 
 def test_stationary_with_cat():
@@ -128,22 +128,40 @@ def test_stationary_with_cat():
 
 
 def test_mixed_kernel_gradients():
-    np.random.seed(40)
+    np.random.seed(42)
+    n = 15
 
-    x_cont = np.random.rand(4, 3)
-    x_cat = np.random.randint(0, 2, size=(4, 2))
+    x_cont = np.sort(np.random.rand(n, 3), 0)
+    x_cat = np.random.randint(0, 4, size=(n, 2))
+    # x_cat = np.arange(2*n).reshape(n ,2)
 
     x = np.hstack((x_cont, x_cat))
-
-    y = np.sum(np.sin(x), 1).reshape(-1, 1)
-    # print(x_cat)
     print(x)
-    k_rbf = GPy.kern.RBF(3, active_dims=[0, 1, 2])
 
-    k = StationaryUniformCat(kernel=k_rbf, cat_dims=[3, 4])
+    y = 1/x.shape[1] * np.sum(np.sin(4*x), 1).reshape(-1, 1) + 0.01 * np.random.randn(len(x), 1)
+    y = (y - np.mean(y))/np.std(y)
+    # print(x_cat)
+    # print(x)
+    k_rbf = GPy.kern.RBF(3, active_dims=[0,1,2])
 
-    gp = GP(x, y, k)
+    k = StationaryUniformCat(kernel=k_rbf, cat_dims=[3,4])
+
+    hp_bounds = np.array([[1., 1.],  # kernel variance
+                          [1e-4, 3],  # lengthscale
+                          # [1e-6, 1e6],  # likelihood variance
+                          ])
+    gp_opt_params = {'method': 'multigrad',
+                     'num_restarts': 10,
+                     'restart_bounds': hp_bounds,
+                     # likelihood variance
+                     'hp_bounds': hp_bounds,
+                     'verbose': False}
+
+    gp = GP(x, y, k, opt_params=gp_opt_params, lik_variance=0.1,
+            lik_variance_fixed=True)
+    print(gp)
     gp.optimize()
+    print(gp)
 
 
 if __name__ == '__main__':