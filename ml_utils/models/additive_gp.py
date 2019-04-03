corrected mixed kernel gradients

ml_utils/models/additive_gp.py
@@ -151,9 +151,11 @@ class MixtureViaSumAndProduct(GPy.kern.Kern):
                     X_sliced[:, q:q + 1] - X2_sliced[:, q:q + 1].T) /
                                     k.lengthscale[q] ** 3
                                     for q in range(k.input_dim)])
+                dk_dl = dk_dr[..., None] * dr_dl
             else:
                 r = k._scaled_dist(X_sliced, X2_sliced)
                 dr_dl = - r / k.lengthscale
+                dk_dl = dk_dr * dr_dl
 
             # # For testing the broadcast multiplication
             # dk_dl_slow = []
@@ -163,8 +165,6 @@ class MixtureViaSumAndProduct(GPy.kern.Kern):
             #     dk_dl_slow.append(dk_dlj)
             #
             # dk_dl_slow = np.dstack(dk_dl_slow)
-
-            dk_dl = dk_dr[..., None] * dr_dl
 
         else:
             raise NotImplementedError
@@ -200,28 +200,30 @@ class MixtureViaSumAndProduct(GPy.kern.Kern):
         # dk/dl for l1 and l2
         # ARD requires a summation along last axis for each lengthscale
         if self.k1.ARD:
-            dk_dl1 = np.sum(dk1_dl1 * (1 - self.mix)
-                            + self.mix * dk1_dl1 * k2_xx[..., None], (0, 1))
+            dk_dl1 = np.sum(dL_dK[..., None] * (dk1_dl1 * (1 - self.mix)
+                                     + self.mix * dk1_dl1 * k2_xx[..., None]),
+                            (0, 1))
         else:
-            dk_dl1 = np.sum(dk1_dl1 * (1 - self.mix)
-                            + self.mix * dk1_dl1 * k2_xx)
+            dk_dl1 = np.sum(dL_dK * (dk1_dl1 * (1 - self.mix)
+                                     + self.mix * dk1_dl1 * k2_xx))
 
         if self.k2.ARD:
-            dk_dl2 = np.sum(dk2_dl2 * (1 - self.mix)
-                            + self.mix * dk2_dl2 * k1_xx[..., None], (0, 1))
+            dk_dl2 = np.sum(dL_dK[..., None] * (dk2_dl2 * (1 - self.mix)
+                                     + self.mix * dk2_dl2 * k1_xx[..., None]),
+                            (0, 1))
         else:
-            dk_dl2 = np.sum(dk2_dl2 * (1 - self.mix)
-                            + self.mix * dk2_dl2 * k1_xx)
+            dk_dl2 = np.sum(dL_dK * (dk2_dl2 * (1 - self.mix)
+                                     + self.mix * dk2_dl2 * k1_xx))
 
         # dk/dvar for var1 and var 2
         if self.fix_variances:
             dk_dvar1 = []
             dk_dvar2 = []
         else:
-            dk_dvar1 = np.sum(dk1_dvar1 * (1 - self.mix)
-                       + self.mix * dk1_dvar1 * k2_xx)
-            dk_dvar2 = np.sum(dk2_dvar2 * (1 - self.mix)
-                       + self.mix * dk2_dvar2 * k1_xx)
+            dk_dvar1 = np.sum(dL_dK * (dk1_dvar1 * (1 - self.mix)
+                                       + self.mix * dk1_dvar1 * k2_xx))
+            dk_dvar2 = np.sum(dL_dK * (dk2_dvar2 * (1 - self.mix)
+                                       + self.mix * dk2_dvar2 * k1_xx))
 
         # Combining the gradients into one vector and updating
         dk_dtheta1 = np.hstack((dk_dvar1, dk_dl1))

tests/models/test_additive_gp.py
@@ -1,3 +1,4 @@
+import sys
 from pprint import pprint
 
 import GPy
@@ -8,6 +9,8 @@ from bayesopt.acquisition import EI
 from ml_utils.models.gp import GP
 from ml_utils.models.additive_gp import AdditiveGP, \
     StationaryUniformCat, MixtureViaSumAndProduct, CategoryOverlapKernel
+
+import numdifftools as nd
 
 #
 #
@@ -474,7 +477,11 @@ def test_kernel_mixture_via_sum_and_product():
     k1 = GPy.kern.RBF(3, active_dims=[0, 1, 2], ARD=True)
     k2 = GPy.kern.Matern52(2, active_dims=[3, 4], ARD=True)
 
-    k = MixtureViaSumAndProduct(5, k1, k2, mix=0.5, fix_variances=False)
+    k = MixtureViaSumAndProduct(5, k1, k2, mix=0., fix_variances=False)
+
+    k3 = GPy.kern.RBF(3, active_dims=[0, 1, 2], ARD=True)
+    k4 = GPy.kern.Matern52(2, active_dims=[3, 4], ARD=True)
+    k_test = k3+k4
 
     hp_bounds = np.array([[1e-4, 3],  # k1
                           [1e-4, 3],  # k2
@@ -496,9 +503,37 @@ def test_kernel_mixture_via_sum_and_product():
     # print(k.param_array)
 
     gp = GP(x, y, k, opt_params=gp_opt_params)
+    gp_test = GP(x, y, k_test, opt_params=gp_opt_params)
     print(gp)
+    print(gp_test)
+    print(gp.gradient)
+    def ll(theta):
+        old_theta = gp.param_array
+        gp.param_array = theta
+        loglik = gp.log_likelihood()
+        gp.param_array = old_theta
+        return loglik
+    G = nd.Gradient(ll, step=0.01)(gp.param_array)
+    print(G)
+
+    print(gp_test.gradient)
+
+    def ll(theta):
+        old_theta = gp_test.param_array
+        gp_test.param_array = theta
+        loglik = gp_test.log_likelihood()
+        gp_test.param_array = old_theta
+        return loglik
+
+    G = nd.Gradient(ll, step=0.01)(gp_test.param_array)
+    print(G)
+    print(np.allclose(gp.gradient, gp_test.gradient))
+
+
     gp.optimize()
     print(gp)
+
+
 
     Kxx = k.K(x, x)
     plt.imshow(Kxx)